3fdd31de643638b6ebb472e1ba1f4a855d29a16e
Sunil Chuhan <141014078+sunilchauhan5790@users.noreply.github.com>
2023-09-15 11:58:26 -0700

    live websites

---
+++ Web.c
@@ -0,0 +1,13 @@
+
+#include <conio.h>
+void main()
+clrscr();
+int s1,s2,s3;
+printf("enter s1=");
+scanf("%d",&s1);
+printf("enter s2=");
+scanf("%d",&s2);
+printf("enter s3=");
+scanf("%d",&s3);
+printf("total=%d",s1+s2+s3);
+getch();
